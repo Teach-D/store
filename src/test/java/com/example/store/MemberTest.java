9948595c4c3a1932dd84734325f124612baf07cf
Teach-D <wkadht0619@gmail.com>
2024-08-11 13:20:26 +0900

    feat: github actions

---
+++ src/test/java/com/example/store/MemberTest.java
@@ -0,0 +1,19 @@
+package com.example.store;
+
+import com.example.store.dto.MemberSignupDto;
+import com.example.store.entity.Member;
+import com.example.store.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class MemberTest {
+
+
+}
