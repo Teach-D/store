8631006d18f2ddd0539aba1d716e89b0efc4dd4b
Teach-D <wkadht0619@gmail.com>
2024-08-18 10:49:30 +0900

    add: github actions test추가

--- src/test/java/com/example/store/MemberTest.java
+++ src/test/java/com/example/store/MemberTest.java
@@ -15,5 +15,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class MemberTest {
 
+    @Autowired
+    private MemberService memberService;
 
+
+
+    @Test
+    @DisplayName("멤버 저장하고 조회하기")
+    public void saveAndFind() {
+        MemberSignupDto memberSignupDto1 = MemberSignupDto.builder().email("wkadht0619@inu.ac.kr").name("aa").password("hhm0310@@").build();
+        MemberSignupDto memberSignupDto2 = MemberSignupDto.builder().email("wkadht06191@inu.ac.kr").name("aa").password("hhm0310@@").build();
+
+        memberService.createMember(memberSignupDto1);
+        memberService.createMember(memberSignupDto2);
+
+        Member memberByEmail1 = memberService.findByEmail(memberSignupDto1.getEmail());
+        Member memberByEmail2 = memberService.findByEmail(memberSignupDto2.getEmail());
+
+        Assertions.assertThat(memberSignupDto1.getEmail()).isEqualTo(memberByEmail1.getEmail());
+    }
 }
