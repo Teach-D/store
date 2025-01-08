d083ce981f20d67a864883784cfde463ed69f667
Teach-D <wkadht0619@gmail.com>
2025-01-08 14:39:31 +0900

    add: ProductTagRepository findByTagIdAndProductId메서드 추가

--- src/main/java/com/example/store/repository/ProductTagRepository.java
+++ src/main/java/com/example/store/repository/ProductTagRepository.java
@@ -3,6 +3,9 @@ package com.example.store.repository;
 import com.example.store.entity.ProductTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductTagRepository extends JpaRepository<ProductTag, Long> {
+    Optional<ProductTag> findByTagIdAndProductId(Long tagId, Long productId);
 
 }
