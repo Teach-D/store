97a431a130f30041267f9dd3b00b954ae7a14934
Teach-D <wkadht0619@gmail.com>
2025-01-19 11:10:52 +0900

    add: findByDiscountName 메서드 추가

--- src/main/java/com/example/store/repository/DiscountRepository.java
+++ src/main/java/com/example/store/repository/DiscountRepository.java
@@ -3,7 +3,8 @@ package com.example.store.repository;
 import com.example.store.entity.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface DiscountRepository extends JpaRepository<Discount, Long> {
+    Optional<Discount> findByDiscountName(String discountName);
 }
