7a1d3da25ba49d6615fe2c4048f12cb5143722bb
Teach-D <wkadht0619@gmail.com>
2025-02-02 13:48:59 +0900

    feat: BaseTimeEntity

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -3,7 +3,9 @@ package com.example.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // BaseTimeEntity를 위한 활성화
 @EnableCaching
 @SpringBootApplication
 public class StoreApplication {
