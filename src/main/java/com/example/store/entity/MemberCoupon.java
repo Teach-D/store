3e0ca9cc2608a15df054871444e2569a2ab64021
Teach-D <wkadht0619@gmail.com>
2025-02-15 13:50:47 +0900

    fix: ObjectMapper 직렬화 해결

--- src/main/java/com/example/store/entity/MemberCoupon.java
+++ src/main/java/com/example/store/entity/MemberCoupon.java
@@ -1,6 +1,8 @@
 package com.example.store.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +17,7 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 @Getter
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class MemberCoupon {
 
     @Id
