ade666b38b0b36dbfab90488bf2d15c8e0e2e16a
Teach-D <wkadht0619@gmail.com>
2025-03-22 10:26:28 +0900

    test: productCacheServiceTest 개발

--- src/main/java/com/example/store/entity/ProductTag.java
+++ src/main/java/com/example/store/entity/ProductTag.java
@@ -2,10 +2,14 @@ package com.example.store.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class ProductTag {
